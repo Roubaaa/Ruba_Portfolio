b572337ccf3a37f9a5118d1389f4a4c2c9f9bbd3
Ruba Alsoheil <rubasoheil@gmail.com>
2023-03-24 14:44:31 +0200

    initializing

---
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar(){
+
+return(
+<div>
+    <ul>
+        <li>About ME</li>
+        <li>Education</li>
+        <li>Experience</li>
+        <li>Contact</li>
+
+    </ul>
+</div>
+
+)
+    }
+
+export default Navbar
